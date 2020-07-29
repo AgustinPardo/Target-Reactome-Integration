079755ec9873b0d660a75df899a3dc7a01b20458
AgustinPardo <agu_pardo@hotmail.com>
2020-07-29 12:26:01 -0300

    Retoque descripcion funciones

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 import argparse
 
 def target_db_retrieve(organismo):
-    """Me conecto a la Target DB y obtengo la data del organismo (Uniprot id, organismo id, organismo Target DB id)"""
+    """Me conecto a la Target DB y obtengo la data del organismo (Uniprot id, proteinas_organismo id)"""
     return 0
 
 def reactome_db_retrieve(input_file):
     #Archivo de entrada UniProt2Reactome_PE_Reactions.txt
-    """Levanto la data de Reactome DB (Uniprot id ,Reactome id)"""
+    """Levanto la data de Reactome DB (Uniprot id, Reactome id)"""
     return 0
 
 def id_cross(organismo_data, reactome_data):
     """Curce de ids entre Reactome DB y Target DB"""
     return 0
 
-def target_db_integration(cruce_ids_data, organismo_target_id):
+def target_db_integration(cruce_ids_data, organismo):
     """Integro el cruce de ids a Target DB"""
     return 0
 
