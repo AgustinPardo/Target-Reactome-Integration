5aef7e6a882cd4a57b9e78d4014406f0e1571627
AgustinPardo <agu_pardo@hotmail.com>
2020-07-29 21:36:03 -0300

    aux commands to database retrieve

--- aux.sh
+++
@@ -1,2 +0,0 @@
-curl -X GET "https://www.uniprot.org/uniprot/?query=organism:5664&columns=id&format=list&compress=no" -H "accept: text/plain" > lmajor_id.txt
-awk '{print $0,NR}' lmajor_id.txt > lmajor_id.tmp && mv lmajor_id.tmp lmajor_id.txt
