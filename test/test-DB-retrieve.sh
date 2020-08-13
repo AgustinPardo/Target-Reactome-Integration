4c9a86996238c51c47f969d8e9a92ba7c96d8811
AgustinPardo <agu_pardo@hotmail.com>
2020-08-13 20:26:36 -0300

    Test finish

--- test/test-DB-retrieve.sh
+++
@@ -1,7 +0,0 @@
-# Download reactome reactions id and uniprotID link Database (UniProt2Reactome_PE_Reactions.txt 161 Mb).
-curl -X GET "https://reactome.org/download/current/UniProt2Reactome_PE_Reactions.txt" -H "accept: text/plain" > UniProt2Reactome_PE_Reactions.txt
-
-# Download organism uniprotID proteome.
-url -X GET "https://www.uniprot.org/uniprot/?query=organism:5664&columns=id&format=list&compress=no" -H "accept: text/plain" > lmajor_id.txt
-# Add an id tag to the each uniprotID.
-awk '{print $0,"lm_"NR}' lmajor_id.txt > lmajor_id.tmp && mv lmajor_id.tmp lmajor_id.txt
